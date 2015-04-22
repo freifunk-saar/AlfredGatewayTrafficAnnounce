edf7cb3c103cba9d8548f86f4c1068ed96cd5ddd
Rugosh <tobias.s.kuhn@gmail.com>
2015-04-22 23:12:00 +0200

    enhanced gw stat annound python for genearting json for the alfred announce

--- announceGatewayTraffic.py
+++ announceGatewayTraffic.py
@@ -3,29 +3,65 @@ import subprocess
 import argparse
 from xml.dom import minidom
 
+class GatewayStat():
+	def __init__(self):
+		self.hour = []
+		self.day = []
+		self.month = []
 
-''' Get programm args '''
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--batman', action='store', help='bat device', default='bat0')
+	def dump(self):
+		dumpRet = {}
+		dumpRet['hour'] = self.hour
+		dumpRet['day'] = self.day
+		dumpRet['month'] = self.month
+		return dumpRet
 
-args = parser.parse_args()
-argsDir = vars(args)
-
-batDev = argsDir['batman']
+def getText(nodelist):
+	retValue = []
+	for node in nodelist:
+		if node.nodeType == node.TEXT_NODE:
+			retValue.append(node.data)
+	return ''.join(retValue)
 
 ''' load traffic stat '''
 statXML = subprocess.check_output(["vnstat","-i","eth0", "--xml"])
 
 
 ''' process traffic stat '''
+gwStat = GatewayStat()
 dom = minidom.parseString(statXML)
 
 months = dom.getElementsByTagName('months')[0].childNodes
-days = dom.getElementsByTagName('day')[0].childNodes
-hours = dom.getElementsByTagName('hour')[0].childNodes
+days = dom.getElementsByTagName('days')[0].childNodes
+hours = dom.getElementsByTagName('hours')[0].childNodes
+
+for hour in hours:
+	if hour.nodeType == hour.ELEMENT_NODE:
+		h = {}
+		h['day'] = getText(hour.getElementsByTagName('day')[0].childNodes)
+		h['id'] = hour.attributes["id"].value
+		h['rx'] = getText(hour.getElementsByTagName('rx')[0].childNodes)
+		h['tx'] = getText(hour.getElementsByTagName('rx')[0].childNodes)
+		gwStat.hour.append(h)
+
+for day in days:
+	if day.nodeType == day.ELEMENT_NODE:
+		d = {}
+		d['day'] = getText(day.getElementsByTagName('day')[0].childNodes)
+		d['month'] = getText(day.getElementsByTagName('month')[0].childNodes)
+                d['rx'] = getText(day.getElementsByTagName('rx')[0].childNodes)
+                d['tx'] = getText(day.getElementsByTagName('rx')[0].childNodes)
+		gwStat.day.append(d)
 
 for month in months:
 	if month.nodeType == month.ELEMENT_NODE:
-		print month
+		m = {}
+		m['year'] = getText(month.getElementsByTagName('year')[0].childNodes)
+		m['month'] = getText(month.getElementsByTagName('month')[0].childNodes)
+		m['rx'] = getText(month.getElementsByTagName('rx')[0].childNodes)
+		m['tx'] = getText(month.getElementsByTagName('tx')[0].childNodes)
+		gwStat.month.append(m)
 
 
+
+print json.dumps(gwStat.dump(), separators=(',',':'))
